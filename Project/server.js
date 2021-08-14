ba1caa8347fd99c716097a63ccd9c20e2ab85879
Sasha <deadsnake555@gmail.com>
2021-08-14 23:11:18 +0300

    Update

--- Project/server.js
+++ Project/server.js
@@ -32,7 +32,7 @@ app.use(passport.session())
 
 
 app.get('/', (req, res)=>{
-    res.redirect('/login')
+    res.render('index.ejs');
 })
 app.get('/home', (req,res)=>{
     res.render('home.ejs', {name: req.user.uname})
